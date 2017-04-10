65a316b8859364e75baf37311f6f6e006c97a488
Chris Targett <chris@xlevus.net>
2017-04-10 15:05:35 +0100

    Moving bulk of command logic into script file

--- dotops/cli.py
+++ dotops/cli.py
@@ -1,18 +1,7 @@
 import logging
-import argparse
 from typing import Union
 
-from .context import context
-from .commands.exec import run_command
-
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--root', metavar='PATH', default='~/.dotops')
-parser.add_argument('--logging', metavar='LEVEL', default=None)
-
-parser.add_argument('command', nargs='?', default='help')
-parser.add_argument('args', nargs=argparse.REMAINDER)
+from dotops.context import context
 
 
 def set_log_level(level: Union[None, str] = None) -> None:
@@ -28,12 +17,3 @@ def set_log_level(level: Union[None, str] = None) -> None:
         context.logging = level
 
     logging.basicConfig(level=level)
-
-
-def cli():
-    args = parser.parse_args()
-
-    context.root = context.get('root', args.root)
-    set_log_level(args.logging)
-
-    run_command(args.command, cli_args=args.args)
